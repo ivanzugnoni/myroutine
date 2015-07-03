9a1e82e50031b381ff759e2094d6ab915fc038ce
Ivan Zugnoni <zugnoni.ivan@gmail.com>
2015-07-03 10:50:22 -0300

    API working for Exercise CRUD

--- management/core/settings.py
+++ management/core/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
     'api',
     'routines',
 )
@@ -103,3 +105,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+}
--- management/routines/models.py
+++ management/routines/models.py
@@ -6,5 +6,6 @@ class Exercise(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
 
+
     def __unicode__(self):
         return self.name
