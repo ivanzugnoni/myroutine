f3b431ec608f5870cb7d40155ee288f79e4c1de2
Ivan Zugnoni <zugnoni.ivan@gmail.com>
2015-07-03 15:40:45 -0300

    Add description to API viewSets

--- management/api/views.py
+++ management/api/views.py
@@ -6,11 +6,39 @@ from rest_framework import viewsets
 
 class ExerciseViewSet(viewsets.ModelViewSet):
 
+    """
+    This endpoint represents Exercises.
+
+    ## Allowed actions:
+
+    *   List all exercises:
+
+        `GET /exercises`
+
+    *   Retrieve one particular exercise:
+
+        `GET /exercises/:id`
+    """
+
     queryset = Exercise.objects.all()
     serializer_class = ExerciseSerializer
 
 
 class RoutineViewSet(viewsets.ModelViewSet):
+
+    """
+    This endpoint represents Routines.
+
+    ## Allowed actions:
+
+    *   List all routines:
+
+        `GET /routines`
+
+    *   Retrieve one particular routine:
+
+        `GET /routines/:id`
+    """
 
     queryset = Routine.objects.all()
     serializer_class = RoutineSerializer
