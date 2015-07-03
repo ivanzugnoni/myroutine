6aa0b87f63ef0b140cec56dbfc1cb31d1dfa3c47
Ivan Zugnoni <zugnoni.ivan@gmail.com>
2015-07-03 11:18:00 -0300

    Add muscle_group field to Exercise model

--- management/routines/models.py
+++ management/routines/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
+MUSCLE_GROUP_CHOICES = (
+    ('pecho', 'Pecho'),
+    ('espalda', 'Espalda'),
+    ('biceps', 'Biceps'),
+)
+
 
 class Exercise(models.Model):
 
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
-
+    muscle_group = models.CharField(max_length=100,
+                                    choices=MUSCLE_GROUP_CHOICES)
 
     def __unicode__(self):
         return self.name
