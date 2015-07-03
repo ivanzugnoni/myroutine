e2f4f23fa4532723d83747cf61e498725c89a433
Ivan Zugnoni <zugnoni.ivan@gmail.com>
2015-07-03 11:31:11 -0300

    Add 'id' field with hash_id to Exercise model

--- management/routines/models.py
+++ management/routines/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import random, string
 
 MUSCLE_GROUP_CHOICES = (
     ('pecho', 'Pecho'),
@@ -6,9 +7,19 @@ MUSCLE_GROUP_CHOICES = (
     ('biceps', 'Biceps'),
 )
 
+def get_hash_id():
+    """Generates random 16 char lowercase string with numbers and letters"""
+    hash_seed = string.ascii_lowercase + string.digits
+    hash_string = ''
+    for x in random.sample(hash_seed, 16):
+        hash_string += x
+    return hash_string
+
 
 class Exercise(models.Model):
 
+    id = models.CharField(primary_key=True, default=get_hash_id, max_length=16,
+                          editable=False)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
     muscle_group = models.CharField(max_length=100,
