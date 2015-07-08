25a9d61786beff292b74d02ab16d4487799be71c
Ivan Zugnoni <zugnoni.ivan@gmail.com>
2015-07-08 10:17:42 -0300

    Add pagination

--- management/core/settings.py
+++ management/core/settings.py
@@ -111,5 +111,5 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 10
 }
